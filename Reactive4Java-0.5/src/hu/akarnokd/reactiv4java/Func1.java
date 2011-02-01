ca0eea580731fb1b149a82335a3a24106d0671a8
akarnokd <akarnokd@148939a6-0e71-623c-e8c4-685086a1d650>
2011-02-01 17:37:20 +0000

    Branch/Tag for the version 0.5 sources.

---
+++ Reactive4Java-0.5/src/hu/akarnokd/reactiv4java/Func1.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2011 David Karnok
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package hu.akarnokd.reactiv4java;
+
+/**
+ * The function interface which takes one parameter and returns something.
+ * @author akarnokd
+ * @param <Return> the return type
+ * @param <Param1> the first parameter
+ */
+public interface Func1<Return, Param1> {
+	/**
+	 * The method that gets invoked with a parameter.
+	 * @param param1 the parameter value
+	 * @return the return object
+	 */
+	Return invoke(Param1 param1);
+}
