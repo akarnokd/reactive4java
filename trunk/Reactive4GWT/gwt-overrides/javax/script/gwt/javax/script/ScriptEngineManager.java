20e2d70fb61b3fa5b295ee4f4e3a17986ce3e5db
akarnokd <akarnokd@148939a6-0e71-623c-e8c4-685086a1d650>
2011-10-10 07:37:29 +0000

    Fixed GWT based javascript engine bindings.

--- trunk/Reactive4GWT/gwt-overrides/javax/script/gwt/javax/script/ScriptEngineManager.java
+++ trunk/Reactive4GWT/gwt-overrides/javax/script/gwt/javax/script/ScriptEngineManager.java
@@ -15,6 +15,8 @@
  */
 package javax.script;
 
+import java.util.Map;
+
 /**
  * The script engine interface for GWT.
  * @author karnok, 2011.02.20.
@@ -34,7 +36,7 @@ public class ScriptEngineManager {
 				public Object eval(String script, Bindings bindings)
 						throws ScriptException {
 					try {
-						return nativeEval(script);
+						return invoke(script);
 					} catch (Throwable t) {
 						throw new ScriptException(t);
 					}
@@ -43,12 +45,69 @@ public class ScriptEngineManager {
 		}
 		return null;
 	}
+	/** The current bind sequence. */
+	int bindSequence;
+	/** 
+	 * Prepare the bindings for the given sequence.
+	 * @param index the sequence number 
+	 */
+    native void prepareOnWindow(int index) /*-{
+    	$wnd["reactive4java_bindings_" + index] = new Array();
+	}-*/;
+    /**
+     * Set a binding variable name and value on the given sequence.
+     * @param index the sequence number
+     * @param name the variable name
+     * @param value the value
+     */
+	native void setOnWindow(int index, String name, Object value) /*-{
+	    $wnd["reactive4java_bindings_" + index][name] = value;
+	}-*/;
 	/**
-	 * Evaluate the given script string.
-	 * @param script the script
+	 * Clear the binding variables on the given sequence number.
+	 * @param index the sequence number
+	 */
+	native void clearOnWindow(int index) /*-{
+	    $wnd["reactive4java_bindings_" + index] = null;
+	}-*/;
+	/**
+	 * Invoke a Javascript script.
+	 * <p>The method auto-boxes {@code boolean} into {@code java.lang.Boolean}
+	 *  and {@code number} into {@code java.lang.Double} objects. 
+	 * @param script the script to execute
 	 * @return the return value
 	 */
-	native Object nativeEval(String script) /*-{
-		return $wnd.eval(script);
+	native Object invoke(String script) /*-{
+	    var result = $wnd.eval(script);
+	    if (typeof(result) == "boolean") {
+	        return result ? @java.lang.Boolean::TRUE : @java.lang.Boolean::FALSE;
+	    } else
+	    if (typeof(result) == "number") {
+	        return @java.lang.Double::valueOf(D)(result);
+	    }
+	    return result;
 	}-*/;
+	/**
+	 * Invoke the script with the given mappings of variables.
+	 * @param script the script to invoke
+	 * @param bindings the variable bindings
+	 * @return the returned value
+	 */
+	public Object invoke(String script, Bindings bindings) {
+	    int seq = bindSequence++;
+	    try {
+	        StringBuilder script2 = new StringBuilder();
+	        prepareOnWindow(seq);
+	        for (Map.Entry<String, Object> e : bindings.entrySet()) {
+	            setOnWindow(seq, e.getKey(), e.getValue());
+	            script2.append("var ").append(e.getKey()).append(" = ")
+	            .append("window[\"reactive4java_bindings_\" + ").append(seq)
+	            .append("][\"").append(e.getKey()).append("\"];\r\n");
+	        }
+	        script2.append("\r\n").append(script);
+	        return invoke(script);
+	    } finally {
+	        clearOnWindow(seq);
+	    } 
+	}
 }
